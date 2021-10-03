74ed677fb58da4a4531f66da9d1d50bffbc549fd
Rico <42716120+Liquid72@users.noreply.github.com>
2021-10-03 16:53:11 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import numpy as np
+import tulipy as ti
+import requests
+import ccxt
+from telebot import types
+from telebot import util
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import requests
+import telebot
+import pandas as pd
+import matplotlib.pyplot as plt
+import talib
+import json
+import numpy
+import schedule
+from time import sleep
